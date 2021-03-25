2d7943a00cc49445345ee03b9728d7a71a667a08
Ben Paget <ben.paget@live.fr>
2021-03-25 00:11:32 +0000

    Front End Auth Intro

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import React, { useEffect, useState } from "react";
 import Authentication from "./components/Authentication";
 import Admin from "./components/Admin";
 
-import { Switch, Route, useHistory, Redirect } from "react-router-dom";
-
 const App = () => {
-  const [credentials, setCredentials] = useState();
-
-  const history = useHistory();
-
-  const handleSetCredentials = (e) => {
-    setCredentials((prevCredentials) => ({
-      ...prevCredentials,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-
+  
   return (
     <>
       <Authentication />
